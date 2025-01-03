d03dbd4c9350ccefb90c4ad66173f397f08fab46
helpdesk - pc <contatoluansantos7@gmail.com>
2025-01-03 13:23:54 -0300

    feat: create project

---
+++ src/main/java/br/devluan/easystock/EasystockApplication.java
@@ -0,0 +1,13 @@
+package br.devluan.easystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasystockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasystockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/devluan/easystock/EasystockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.devluan.easystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasystockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
